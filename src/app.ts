4758b4eb1a79c2c97ed668b436c461269079d9a1
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-05-19 20:47:15 +0200

    feat: get notion's tasks and calendar's events

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
-import {getTasksFromNotionDatabase, login} from "./notion";
+import {getTasksFromNotionDatabase, login, NotionClient} from "./notion";
 import 'dotenv/config'
+import {authorize, CalendarClient, listEvents} from "./calendar";
+
+
+async function main({notion, gCalendar}: {
+    notion: NotionClient,
+    gCalendar: CalendarClient
+}) {
+    const numberOfDaysInPast = parseInt(process.env.NUMBER_OF_DAYS_IN_PAST)
+
+    // fetch relevant notion tasks
+    const notionTasks = await getTasksFromNotionDatabase(notion, process.env.NOTION_DATABASE_ID, numberOfDaysInPast)
+    console.log(`${notionTasks.length} tasks fetched from Notion.`)
+
+    // fetch relevant google calendar events
+    const gCalendarEvents = await listEvents(gCalendar, process.env.CALENDAR_ID, numberOfDaysInPast)
+    console.log(`${gCalendarEvents.length} tasks fetched from Google Calendar.`)
+
+    // TODO figure out notionTasks/gCalendarEvents association
+    // TODO find modification
+    // TODO find where to update (given the lastEditedTime)
+    // TODO update all elements
+    console.debug("hey")
+
+}
+
 
 login(process.env.NOTION_TOKEN)
-    .then((notion) => getTasksFromNotionDatabase(notion, process.env.NOTION_DATABASE_ID, 5))
-    .then(console.log)
+    .then(async (notion) => ({notion, gCalendar: await authorize()}))
+    .then(main)
     .catch(console.error)
