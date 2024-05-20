78f132c5afe62280bfcf84b275a2bda1df36f4af
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-05-20 20:43:31 +0200

    feat: find events and tasks to delete or update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {getTasksFromNotionDatabase, login, NotionClient} from "./notion";
+import {getTasksFromNotionDatabase, login, NotionClient, NotionObject} from "./notion";
 import 'dotenv/config'
 import {authorize, CalendarClient, CalendarObject, createEvent, listEvents} from "./calendar";
 
@@ -43,6 +43,23 @@ function propertiesToEventDescription(properties: { [key: string]: string }): st
     return description;
 }
 
+/**
+ * Convert a NotionObject to a CalendarObject
+ * @param t the object to convert
+ */
+function notionObjectToCalendarObject(t: NotionObject): CalendarObject {
+    return {
+        name: t.name,
+        description: propertiesToEventDescription({
+            Priority: t.priority,
+            Deadline: t.deadline ? t.deadline.start.toISOString() : 'null',
+            Category: t.category,
+            Archived: t.archived ? "true" : "false",
+            Id: t.id,
+        }),
+        date: t.date
+    }
+}
 
 async function main({notion, gCalendar}: {
     notion: NotionClient,
@@ -55,40 +72,86 @@ async function main({notion, gCalendar}: {
     console.log(`${notionTasks.length} tasks fetched from Notion.`)
 
     // fetch relevant google calendar events
-    const gCalendarEvents = await listEvents(gCalendar, process.env.CALENDAR_ID, numberOfDaysInPast)
-    console.log(`${gCalendarEvents.length} tasks fetched from Google Calendar.`)
+    const calendarEvents = await listEvents(gCalendar, process.env.CALENDAR_ID, numberOfDaysInPast)
+    console.log(`${calendarEvents.length} tasks fetched from Google Calendar.`)
 
     // find element(s) missing in calendar
     const toCreate: CalendarObject[] = notionTasks
-        .filter((task) => gCalendarEvents.find((e) => eventDescriptionToProperties(e.description).id === task.id) == undefined)
-        .map((task) => ({
-            name: task.name,
-            description: propertiesToEventDescription({
-                Priority: task.priority,
-                Deadline: task.deadline ? task.deadline.start.toISOString() : 'null',
-                Category: task.category,
-                Archived: task.archived ? "true" : "false",
-                Id: task.id,
-            }),
-            date: task.date
-        }))
+        .filter((task) => calendarEvents.find((e) => eventDescriptionToProperties(e.description).id === task.id) === undefined)
+        .map((task) => notionObjectToCalendarObject(task))
 
     // send request for elements creation
     if (toCreate.length > 0) {
-        console.log("The following tasks need to be created:\n\t", toCreate.map((e) => e.name).join('; '))
+        console.log("The following events need to be created:", toCreate.map((e) => e.name).join('; '))
         await Promise.all(toCreate.map((e) => createEvent(gCalendar, process.env.CALENDAR_ID, e)))
     } else {
-        console.log("No task needs to be created")
+        console.log("No event needs to be created")
     }
 
-    // TODO find element(s) to delete
-    const toDelete = []
+    // find element(s) to delete
+    const toDelete: CalendarObject[] = calendarEvents.filter((e) => notionTasks.find((t) => t.id === eventDescriptionToProperties(e.description).id) === undefined)
+    // send request for elements deletion
+    if (toDelete.length > 0) {
+        console.log("The following events need to be deleted:", toDelete.map((e) => e.name).join('; '))
+        // TODO delete
+    } else {
+        console.log("No event needs to be deleted")
+    }
 
-    // TODO find modification
-    // TODO find where to update (given the lastEditedTime)
-    const toUpdateNotion = []
-    const toUpdateCalendar = []
-    // TODO update all elements
+    // find elements that needs to be update in notion
+    const toUpdateNotion: NotionObject[] = notionTasks
+        .map((task) => {
+            const event = calendarEvents.find((e) => eventDescriptionToProperties(e.description).id === task.id)
+            // if the event doesn't exist or the update date is before the update date of the task, then
+            // nothing to update related to this event in notion
+            if (!event || event.lastEditedTime < task.lastEditedTime)
+                return null
+
+            if (task.date.start.getTime() !== event.date.start.getTime()
+                || task.date.end?.getTime() !== event.date.end?.getTime()
+                || task.date.isDateTime !== event.date.isDateTime) {
+                return {
+                    ...task,
+                    date: event.date
+                }
+            }
+            return null
+        })
+        .filter((t) => t !== null)
+    // send request for elements update in notion
+    if (toUpdateNotion.length > 0) {
+        console.log("The following tasks need to be updated:", toUpdateNotion.map((t) => t.name).join('; '))
+        // TODO update
+    } else {
+        console.log("No task needs to be updated")
+    }
+
+    // find elements that needs to be update in calendar
+    const toUpdateCalendar: CalendarObject[] = calendarEvents
+        .map((event) => {
+            const task = notionTasks.find((t) => t.id === eventDescriptionToProperties(event.description).id)
+            // if the task doesn't exist or the update date is after the update date of the task, then
+            // nothing to update related to this event in notion
+            if (!task || event.lastEditedTime > task.lastEditedTime)
+                return null
+
+            const taskToEvent = notionObjectToCalendarObject(task)
+            if (event !== taskToEvent) {
+                return {
+                    id: event.id,
+                    ...taskToEvent
+                }
+            }
+            return null
+        })
+        .filter((t) => t !== null)
+    // send request for elements update in calendar
+    if (toUpdateCalendar.length > 0) {
+        console.log("The following events need to be updated:", toUpdateCalendar.map((e) => e.name).join('; '))
+        // TODO update
+    } else {
+        console.log("No event needs to be updated")
+    }
 }
 
 
